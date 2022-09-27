99c470dc12f9be1c4f4895cfb6430dd48bd3a630
mjmoshiri <thisismjmoshiri@gmail.com>
2022-09-28 00:45:24 +0300

    adds quote api to the code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      q: [{text:"",author:""}]
+    }
+  }
+  componentDidMount() {
+    console.log()
+    fetch("https://type.fit/api/quotes")
+      .then(response => response.json())
+      .then((q) => {
+        this.setState({ q: q.map(e=>this.jsObject(e)) });
+      },
+        (error) => {
+          alert(error);
+        })
+  }
+  jsObject(e) {
+    return {
+      text: e.text,
+      author: e.author
+    }
+  }
+  render() {
+    const num =Math.floor(Math.random() * this.state.q.length)
+    return (
+      <div>
+        <p>
+          {this.state.q[num].text}
+          <br/>
+          {this.state.q[num].author}
+        </p>
+      </div>
+    );
+  }
 }
 
 export default App;
