fe4b4196ab0c032e20ddbf97591ab8228acd76a0
mjmoshiri <thisismjmoshiri@gmail.com>
2022-10-01 02:28:25 +0300

    compelets the code adds new Quote button, tweet button and refactors QuoteBox structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,45 @@
 import './index.scss';
 import React from 'react';
 import QuoteBox from './QuoteBox';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faQuoteLeft, faQuoteRight } from '@fortawesome/free-solid-svg-icons'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 interface Quote {
   text: string
   author: string
 }
-class App extends React.Component<{}, { quotes: Quote[] }> {
+class App extends React.Component<{}, { quotes: Quote[], randNum: number }> {
   constructor(props: any) {
     super(props);
     this.state = {
-      quotes: [{ text: "", author: "" }]
+      quotes: [{ text: "", author: "" }],
+      randNum: 0
     }
+    this.handleNewQuote = this.handleNewQuote.bind(this);
   }
   componentDidMount() {
-    console.log()
     fetch("https://type.fit/api/quotes")
       .then(response => response.json())
       .then((q) => {
         this.setState({ quotes: q });
+        this.setState({ randNum: Math.floor(Math.random() * q.length) });
       },
         (error: Error) => {
           alert(error);
         })
   }
-  render() {
+  handleNewQuote() {
     const randNum = Math.floor(Math.random() * this.state.quotes.length)
-    const randQuote = this.state.quotes[randNum]
-    console.log(randQuote)
+    this.setState({ randNum: randNum })
+  }
+  render() {
+    const twitterLink= 'https://twitter.com/intent/tweet?text='+this.state.quotes[this.state.randNum].text+'%0A'+(this.state.quotes[this.state.randNum].author ? '-' +this.state.quotes[this.state.randNum].author :'')
     return (
-      <div >
-        <QuoteBox q={randQuote}></QuoteBox>
+      <div className="QuoteBox" id="quote-box" >
+        <QuoteBox q={this.state.quotes[this.state.randNum]}></QuoteBox>
+        <br/>
+        <button onClick={this.handleNewQuote} id="new-quote" className="NewQuote"><FontAwesomeIcon icon={faQuoteLeft} />New Quote<FontAwesomeIcon icon={faQuoteRight} /></button>
+        <a href={twitterLink} target="_blank" rel="noopener noreferrer" id="tweet-quote" className="Tweet"><FontAwesomeIcon icon={faTwitter} /></a>
       </div>
     );
   }
--- src/QuoteBox.tsx
+++ src/QuoteBox.tsx
@@ -3,18 +3,18 @@ interface Quote {
     text: string
     author: string
 }
-class QuoteBox extends React.Component<{q:Quote}>{
+class QuoteBox extends React.Component<{ q: Quote }>{
     componentDidMount(): void {
         this.setState({ quote: this.props.q });
     }
     render(): React.ReactNode {
-        return (<div id="quote-box" className="QuoteBox">
+        return (
+        <div className="TextBox">
             <div id="text">
                 {this.props.q.text}
             </div>
-            <br />
-            <div id="author" style={{textAlign: "right"}}>
-                - {this.props.q.author}
+            <div id="author" style={{ textAlign: "right" }}>
+                {this.props.q.author ? "-" :""}{this.props.q.author} 
             </div>
         </div>)
     }
